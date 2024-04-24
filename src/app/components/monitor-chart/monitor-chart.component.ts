9479f1cf9d1ff1eaa9d495280c34b5e64b5fd8ba
Illia Khovanskyi <saskiuhia@gmail.com>
2024-04-24 12:32:40 +0200

    Fixed chart legend max height

--- src/app/components/monitor-chart/monitor-chart.component.ts
+++ src/app/components/monitor-chart/monitor-chart.component.ts
@@ -72,7 +72,7 @@ export class MonitorChartComponent implements AfterViewInit {
           plugins: {
             legend: {
               position: "bottom",
-              maxHeight: 400,
+              maxHeight: 600,
               onClick: (ce, li, le) => {
                 // TODO: add dataset hide state save
                 Chart.defaults.plugins.legend.onClick.call(le, ce, li, le);
